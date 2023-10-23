e53c09aac048f2603fbbe34658a0b2a1b221b214
efetekce <efete_kce@proton.me>
2023-10-24 00:23:11 +0300

    C# Dersleri 8: Byte Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,11 +4,14 @@
     // Console.ReadLine() waits for a user input.
 
     // value types
+    byte number4 = 0;
+    // byte type can hold a value between 0 and 255.
+    short number3 = -32768;
     int number1 = 10;
     // all data types have limits to their values.
     long number2 = 324893423423412312;
-    
     Console.WriteLine("Number 1 is {0}", number1);
     Console.WriteLine("Number 2 is {0}", number2);
+    Console.WriteLine("Number 2 is {0}", number3);
     Console.ReadLine();
 }
