a37923d0a409aefa0091dddd7e21f4fedf74fb44
efetekce <efete_kce@proton.me>
2023-10-24 00:45:33 +0300

    C# Dersleri 10: Char Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,20 +1,21 @@
-﻿static void Main(string[] args)
-{
-    //Console.WriteLine("Hello, World!");
-    // Console.ReadLine() waits for a user input.
+﻿
+//Console.WriteLine("Hello, World!");
+// Console.ReadLine() waits for a user input.
 
-    // value types
-    bool condition = true;
-    byte number4 = 0;
-    // byte type can hold a value between 0 and 255.
-    short number3 = -32768;
-    int number1 = 10;
-    // all data types have limits to their values.
-    long number2 = 324893423423412312;
-    Console.WriteLine("Number 1 is {0}", number1);
-    Console.WriteLine("Number 2 is {0}", number2);
-    Console.WriteLine("Number 3 is {0}", number3);
-    Console.WriteLine("Number 4 is {0}", number4);
-
-    Console.ReadLine();
-}
+// value types
+char character = 'e';
+// char type can converted into int type(ascii value).
+string city = "Istanbul";
+bool condition = true;
+byte number4 = 0;
+// byte type can hold a value between 0 and 255.
+short number3 = -32768;
+int number1 = 10;
+// all data types have limits to their values.
+long number2 = 324893423423412312;
+Console.WriteLine("Number 1 is {0}", number1);
+Console.WriteLine("Number 2 is {0}", number2);
+Console.WriteLine("Number 3 is {0}", number3);
+Console.WriteLine("Number 4 is {0}", number4);
+Console.WriteLine("Character is: {0}", character);
+Console.ReadLine();
