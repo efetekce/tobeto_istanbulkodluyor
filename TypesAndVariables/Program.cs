db60cc9858e6cd4f26851a26497062d204615446
efetekce <efete_kce@proton.me>
2023-10-23 23:52:47 +0300

    C# Dersleri 6: Long Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -1,9 +1,11 @@
-﻿
-//Console.WriteLine("Hello, World!");
-// Console.ReadLine() waits for a user input.
+﻿static void Main(string[] args)
+{
+    //Console.WriteLine("Hello, World!");
+    // Console.ReadLine() waits for a user input.
 
-// value types
-int number1 = 10;
-// all data types have limits to their values. 
-Console.WriteLine("Number 1 is {0}", number1);
-Console.ReadLine();
+    // value types
+    int number1 = 10;
+    // all data types have limits to their values. 
+    Console.WriteLine("Number 1 is {0}", number1);
+    Console.ReadLine();
+}
