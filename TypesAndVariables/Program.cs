ba0c80e32a33812c5d542665fcfe9e93b4028f78
efetekce <efete_kce@proton.me>
2023-10-24 00:22:13 +0300

    C# Dersleri 7: Short Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -5,7 +5,10 @@
 
     // value types
     int number1 = 10;
-    // all data types have limits to their values. 
+    // all data types have limits to their values.
+    long number2 = 324893423423412312;
+    
     Console.WriteLine("Number 1 is {0}", number1);
+    Console.WriteLine("Number 2 is {0}", number2);
     Console.ReadLine();
 }
