aabfbe6a03ae0935aafe33369bd4385a38eeecb3
efetekce <efete_kce@proton.me>
2023-10-24 12:48:06 +0300

    C# Dersleri 23: Default Parametreli Metotlarla Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -2,6 +2,7 @@
 var result = Plus(2,4);
 Console.WriteLine(result);
 Console.WriteLine(Plus(33, 5));
+//directly outputting the function
 Console.ReadLine();
 
 static void Add()
@@ -9,8 +10,8 @@ static void Add()
     Console.WriteLine("Added");
 }
 
-static int Plus(int n1, int n2)
+static int Plus(int n1, int n2=22)
 {
     return n1 + n2;
 }
-
+//working with default parameters
