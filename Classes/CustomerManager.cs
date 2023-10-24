7338127c45915b546715e30fe04444e9075ebd35
efetekce <efete_kce@proton.me>
2023-10-24 17:51:21 +0300

    C# Dersleri 37: Classlara Giriş

---
+++ Classes/CustomerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Classes
+{
+   public class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated");
+        }
+    }
+}
+
---
+++ Classes/Program.cs
@@ -0,0 +1,13 @@
+﻿using Classes;
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Update();
+
+Console.ReadLine();
+
+
