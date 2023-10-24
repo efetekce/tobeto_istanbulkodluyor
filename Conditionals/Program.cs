f69ddf3b2a46e16c01b71ed2fa5d657fa5217420
efetekce <efete_kce@proton.me>
2023-10-24 12:04:05 +0300

    C# Dersleri 17: Else If Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,13 +1,17 @@
-﻿var number = 4;
-//if (number == 8)
-//{
-//    Console.WriteLine("number is 8");
-//}
-//else
-//{
-//    Console.WriteLine("number is not 8");
-//}
+﻿var number = 8;
+if (number == 8)
+{
+    Console.WriteLine("number is 8");
+}
+else if(number > 0)
+{
+    Console.WriteLine("number is greater than 0");
+}
+else
+{
+    Console.WriteLine("number is neither");
+}
 
-Console.WriteLine(number == 8 ? "number is 8" : "number is not 8");
+//Console.WriteLine(number == 8 ? "number is 8" : "number is not 8");
 
 Console.ReadLine();
