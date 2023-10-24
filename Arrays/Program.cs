b7e05b0f400cf5c0599533ff6d96429d1a54262c
efetekce <efete_kce@proton.me>
2023-10-24 13:19:11 +0300

    C# Dersleri 28: Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string[] students = new string[3];
+students[0] = "Efe";
+students[1] = "Efe";
+students[2] = "Efe";
+string[] fruits = { "apple", "banana", "orange" };
+foreach (var fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+Console.WriteLine();
+Console.ReadLine();
+
