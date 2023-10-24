ca9c112a3190c87d3eda45086084752cb35d5b5a
efetekce <efete_kce@proton.me>
2023-10-24 16:39:55 +0300

    C# Dersleri 32: Do While Döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,17 +1,32 @@
-﻿//ForLoop();
-//Console.WriteLine("finished");
+﻿//WhileLoop();
+//ForLoop();
 
-int a = 98;
-while ( a > 0){
-    Console.WriteLine(a);
-    a--;
-}
+
+int num = 10;
+
+do
+{
+    Console.WriteLine(num);
+    num--;
+} while (num > 0);
+//do executes once before the condition.
 Console.ReadLine();
+
 
 static void ForLoop()
 {
     for (int i = 255; i > 10; i -= 4)
     {
         Console.WriteLine(i);
+    }
+}
+
+static void WhileLoop()
+{
+    int a = 98;
+    while (a > 0)
+    {
+        Console.WriteLine(a);
+        a--;
     }
 }
