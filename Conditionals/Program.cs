0e1e71e6af41b42b4b21c50aaa176cd7b3273079
efetekce <efete_kce@proton.me>
2023-10-24 12:00:33 +0300

    C# Dersleri 16: Single Line If İle Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,11 +1,13 @@
 ﻿var number = 4;
-if (number == 8)
-{
-    Console.WriteLine("number is 8");
-}
-else
-{
-    Console.WriteLine("number is not 8");
-}
+//if (number == 8)
+//{
+//    Console.WriteLine("number is 8");
+//}
+//else
+//{
+//    Console.WriteLine("number is not 8");
+//}
+
+Console.WriteLine(number == 8 ? "number is 8" : "number is not 8");
 
 Console.ReadLine();
