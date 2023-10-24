4b27c275a71a8318663305127d99ac5c70ac293d
efetekce <efete_kce@proton.me>
2023-10-24 17:26:22 +0300

    gitignore problem fix due to folder

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,10 +1,21 @@
-﻿string city = "Istanbul";
-//Console.WriteLine(city[0]);
-string city2 = "Antalya";
-foreach (var item in city)
+﻿//StringIntro();
+
+string sentence = "Getting good at c#";
+
+var result = sentence.Length;
+var result2 = sentence.Clone();
+sentence = "Syntax is similar to javascript";
+
+static void StringIntro()
 {
-    Console.WriteLine(item);
+    string city = "Istanbul";
+    //Console.WriteLine(city[0]);
+    string city2 = "Antalya";
+    foreach (var item in city)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine(city + city2);
+    Console.WriteLine(String.Format("{1} {0}", city, city2));
+    Console.ReadLine();
 }
-Console.WriteLine(city + city2);
-Console.WriteLine(String.Format("{1} {0}", city, city2));
-Console.ReadLine();
