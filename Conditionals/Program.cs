5766364b608f19f782b7d5c6fad68b5c86ee0fc7
efetekce <efete_kce@proton.me>
2023-10-24 12:16:19 +0300

    C# Dersleri 19: Demo: Çoklu Şartlarla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -13,16 +13,29 @@
 //}
 
 
-switch (number)
-{
-    case 10:
-        Console.WriteLine("number is 10");
-        break;
-    default:
-        Console.WriteLine("number is not 10");
-        break;
-}
+//switch (number)
+//{
+//    case 10:
+//        Console.WriteLine("number is 10");
+//        break;
+//    default:
+//        Console.WriteLine("number is not 10");
+//        break;
+//}
 
 //Console.WriteLine(number == 8 ? "number is 8" : "number is not 8");
 
+
+if (number >= 0 && number <= 100)
+{
+    Console.WriteLine("number is between 0-100");
+}
+else if (number > 100 && number <= 200)
+{
+    Console.WriteLine("number is between 101-200");
+}
+else if (number > 200 || number < 0)
+{
+    Console.WriteLine("number is less than 0 or greater than 101-200");
+}
 Console.ReadLine();
