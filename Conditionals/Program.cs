c70c9b96d8dce3035a96f32e51591c9eb11527c2
efetekce <efete_kce@proton.me>
2023-10-24 12:09:43 +0300

    C# Dersleri 18: Switch Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,15 +1,26 @@
 ﻿var number = 8;
-if (number == 8)
+//if (number == 8)
+//{
+//    Console.WriteLine("number is 8");
+//}
+//else if(number > 0)
+//{
+//    Console.WriteLine("number is greater than 0");
+//}
+//else
+//{
+//    Console.WriteLine("number is neither");
+//}
+
+
+switch (number)
 {
-    Console.WriteLine("number is 8");
-}
-else if(number > 0)
-{
-    Console.WriteLine("number is greater than 0");
-}
-else
-{
-    Console.WriteLine("number is neither");
+    case 10:
+        Console.WriteLine("number is 10");
+        break;
+    default:
+        Console.WriteLine("number is not 10");
+        break;
 }
 
 //Console.WriteLine(number == 8 ? "number is 8" : "number is not 8");
