9ca63095b0b28f1d4c2972c1d42faca31259c293
efetekce <efete_kce@proton.me>
2023-10-24 12:56:00 +0300

    C# Dersleri 24: Challenge : Ref Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -3,6 +3,10 @@ var result = Plus(2,4);
 Console.WriteLine(result);
 Console.WriteLine(Plus(33, 5));
 //directly outputting the function
+int n1 = 1;
+int n2 = 44;
+var result2 = Plus3(ref n1, n2);
+Console.WriteLine(result2);
 Console.ReadLine();
 
 static void Add()
@@ -15,3 +19,9 @@ static int Plus(int n1, int n2=22)
     return n1 + n2;
 }
 //working with default parameters
+
+static int Plus3(ref int n1,int n2)
+{
+    n1 = 30;
+    return n1 + n2;
+}
