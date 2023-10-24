5ba97cc102daa7ba5efd4915731faad985e0a713
efetekce <efete_kce@proton.me>
2023-10-24 13:33:16 +0300

    C# Dersleri 29: Çok Boyutlu Diziler

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -3,12 +3,29 @@ string[] students = new string[3];
 students[0] = "Efe";
 students[1] = "Efe";
 students[2] = "Efe";
-string[] fruits = { "apple", "banana", "orange" };
-foreach (var fruit in fruits)
-{
-    Console.WriteLine(fruit);
-}
+//string[] fruits = { "apple", "banana", "orange" };
+//foreach (var fruit in fruits)
+//{
+//    Console.WriteLine(fruit);
+//}
 
+
+string[,] regions = new string[4, 3]
+{
+    {"Istanbul", "Zonguldak", "Mersin" },
+    {"Agri", "Usak", "Antep" },
+    {"Izmir", "Ankara", "Antalya" },
+    {"Kirklareli", "Adiyaman", "Mugla" }
+};
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i, j]);
+    }
+    Console.WriteLine("////////////////");
+}
 Console.WriteLine();
 Console.ReadLine();
 
